c6af25c717c75a533d7cf0be331a52195cd23c99
Pratip Kayal <pratipkayal1@gmail.com>
2024-06-28 20:40:21 +0530

    commit

--- config/server.js
+++ config/server.js
@@ -1,13 +1,17 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  logger: {
-    level: 'debug', // Set logging level here ('debug' or 'trace' for more verbose output)
-  },
-});
+module.exports = ({ env }) => {
+  console.log("App Keys: ", env.array('APP_KEYS'));
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+    logger: {
+      level: 'debug', // Set logging level here ('debug' or 'trace' for more verbose output)
+    },
+  };
+};
